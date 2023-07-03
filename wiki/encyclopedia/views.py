dc2bc805227468cf8f2e336fc33af49c5dbe891f
Kero <keroagaiby@gmail.com>
2023-07-03 15:31:52 -0700

    added search

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -4,18 +4,22 @@ import markdown2 as markdown
 
 from . import util
 
+#convert markdown to HTML
 def convert_md_to_html(title):
     content = util.get_entry(title)
     markdowner = markdown.Markdown()
     if content == None:
         return None
     else:
-        markdowner.convert(content) #fix this line
+        return markdowner.convert(content) #fix this line
 
 def index(request):
+    entries = util.list_entries()
+    css_file = util.get_entry("CSS")
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
 
 #check if the entry exists
 def entry(request, title):
@@ -25,4 +29,23 @@ def entry(request, title):
             "error": "Error: Page not found"
         })
     else:
-        return render(request, "encyclopedia/entry.html")
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": html_content
+        })
+    
+
+#search function
+
+def search(request):
+    if request.method == "POST":
+        entry_search = request.POST['q']
+        html_content = convert_md_to_html(entry_search)
+        if html_content is not None:
+            return render(request, "encyclopedia/entry.html", {
+                "title": entry_search,
+                "content": html_content
+        })
+    
+
+         
