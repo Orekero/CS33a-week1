a2e1f849362b072432c6be21848a787f571f7291
Kero <keroagaiby@gmail.com>
2023-07-03 09:17:37 -0700

    save

---
+++ wiki/encyclopedia/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+import markdown
+
+from . import util
+
+def convert_md_to_html(title):
+    content = util.get_entry(title)
+    markdowner = markdown.Markdown()
+    if content == None:
+        return None
+    else:
+        markdowner.convert(content) #fix this line
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+#check if the entry exists
+def entry(request, title):
+    html_content = convert_md_to_html(title)
+    if html_content == None:
+        return render(request, "encyclopedia/error.html", {
+            "error": "Error: Page not found"
+        })
+    else:
+        return render(request, "encyclopedia/entry.html")
